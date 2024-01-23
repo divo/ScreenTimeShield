2edf8e415ea079ce4670214b274c096a7568bbd1
divo <divoapple@gmail.com>
2024-01-23 10:51:02 +0000

    Allow apps to be added to restriction inside the active period

--- ScreenTimeShield/ContentView.swift
+++ ScreenTimeShield/ContentView.swift
@@ -8,58 +8,72 @@
 import SwiftUI
 import FamilyControls
 import Foundation
+import AlertToast
 
 struct ContentView: View {
   @State private var isShowingRestrict = false
   
   @EnvironmentObject var model: Model
+  @State var showToast: Bool = false
  
   var body: some View {
     NavigationView {
-      VStack {
-        HStack {
-          Text("Unskippable app limits").padding(.horizontal)
-          Spacer()
-        }
-        
-        if model.selectionToRestrict.applicationTokens.count != 0 {
-          Text("You have restricted \(model.selectionToRestrict.applicationTokens.count) apps and \(model.selectionToRestrict.webDomainTokens.count) websites").padding(20)
-//          Text(model.selectionToRestrict.applications.first?.localizedDisplayName ?? "")
-        }
-        
+      ZStack {
         VStack {
-          DatePicker("Schedule start", selection: $model.start, displayedComponents: .hourAndMinute)
-            .disabled(model.insideInterval)
-            .foregroundColor(model.insideInterval ? Color(uiColor: .systemGray) : .primary)
-          DatePicker("Schedule End", selection: $model.end, displayedComponents: .hourAndMinute)
-            .disabled(model.insideInterval)
-            .foregroundColor(model.insideInterval ? Color(uiColor: .systemGray) : .primary)
-        }.padding(20)
-        Button("Select apps to restrict") {
-          isShowingRestrict = true
-        }.disabled(model.insideInterval)
+          HStack {
+            Text("Unskippable app limits").padding(.horizontal)
+            Spacer()
+          }
+          
+          if model.selectionToRestrict.applicationTokens.count != 0 {
+            Text("You have restricted \(model.selectionToRestrict.applicationTokens.count) apps and \(model.selectionToRestrict.webDomainTokens.count) websites").padding(20)
+          }
+          
+          VStack {
+            DatePicker("Schedule start", selection: $model.start, displayedComponents: .hourAndMinute)
+              .disabled(model.insideInterval)
+              .foregroundColor(model.insideInterval ? Color(uiColor: .systemGray) : .primary)
+            DatePicker("Schedule End", selection: $model.end, displayedComponents: .hourAndMinute)
+              .disabled(model.insideInterval)
+              .foregroundColor(model.insideInterval ? Color(uiColor: .systemGray) : .primary)
+          }.padding(20)
+          Button(model.insideInterval ? "Add apps to restriction" : "Select apps to restrict") {
+            isShowingRestrict = true
+          }
+          //        }.disabled(model.insideInterval)
           .familyActivityPicker(isPresented: $isShowingRestrict, selection: $model.selectionToRestrict)
           .padding(20)
           .controlSize(.large)
           .foregroundColor(.white)
           .buttonStyle(.borderedProminent)
           .tint(Style.primaryColor)
-        if model.insideInterval {
-          HStack {
-            Image(systemName: "exclamationmark.lock").foregroundColor(Color(uiColor: .systemPink))
-            Text("Limits are locked when active")
+          if model.insideInterval {
+            HStack {
+              Image(systemName: "exclamationmark.lock").foregroundColor(Color(uiColor: .systemPink))
+              Text("Limits are locked when active. Apps can still be added to restriction")
+            }
+          } else {
+            HStack {
+              Image(systemName: "lock.open.trianglebadge.exclamationmark").foregroundColor(Color(uiColor: .systemPink))
+              Text("Limits will be locked when active")
+            }
           }
-        } else {
-          HStack {
-            Image(systemName: "lock.open.trianglebadge.exclamationmark").foregroundColor(Color(uiColor: .systemPink))
-            Text("Limits will be locked when active")
-          }
+          Spacer()
+          
         }
-        Spacer()
-        
-      }.onChange(of: model.selectionToRestrict) { newValue in
-        model.saveSelection()
-        Schedule.setSchedule(start: model.start, end: model.end, event: model.activityEvent())
+      }.toast(isPresenting: $showToast, alert: {
+        AlertToast(displayMode: .alert, type: .error(Style.errorColor), title: "Cannot remove apps from block")
+      })
+      .onChange(of: model.selectionToRestrict) { newValue in
+        if model.validateRestriction() {
+          model.saveSelection()
+          Schedule.setSchedule(start: model.start, end: model.end, event: model.activityEvent())
+        } else {
+          // TODO: Show warning
+          print("Cannot remove apps from restrictions")
+          showToast = true
+          model.loadSelection()
+        }
       }.onChange(of: model.start) { newValue in
         if !model.isEmpty() {
           Schedule.setSchedule(start: model.start, end: model.end, event: model.activityEvent())
--- ScreenTimeShield/Style.swift
+++ ScreenTimeShield/Style.swift
@@ -10,4 +10,8 @@ import SwiftUI
 struct Style {
   static let primaryColor = Color(red: 105 / 255, green: 87 / 255, blue: 232 / 255)
   static let primaryUIColor = UIColor(red: 105 / 255, green: 87 / 255, blue: 232 / 255, alpha: 1.0)
+  static let backgroundColor = Color(red: 94 / 255, green: 208 / 255, blue: 250 / 255)
+  static let errorColor = Color(red: 248 / 255, green: 106 / 255, blue: 106 / 255)
+  static let fontColor = Color(red: 240 / 255, green: 244 / 255, blue: 248 / 255)
+  static let fontUIColor = UIColor(red: 240 / 255, green: 244 / 255, blue: 248 / 255, alpha: 1.0)
 }
